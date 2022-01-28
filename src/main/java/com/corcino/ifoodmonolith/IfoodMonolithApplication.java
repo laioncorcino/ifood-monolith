9e27e5384d4e386c884d4e952c73bfe856744e37
Laion Corcino <laioncorcino@gmail.com>
2022-01-28 15:57:04 -0300

    feat: Initial commit

---
+++ src/main/java/com/corcino/ifoodmonolith/IfoodMonolithApplication.java
@@ -0,0 +1,13 @@
+package com.corcino.ifoodmonolith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfoodMonolithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IfoodMonolithApplication.class, args);
+	}
+
+}
